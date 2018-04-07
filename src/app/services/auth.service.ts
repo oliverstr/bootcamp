810e1e3d148f5319d28d30abb82fab9c508f36d6
Oliver Strobel <oliver.strobel@venturus.org.br>
2018-04-07 17:38:14 -0300

    firebase login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Injectable()
 export class AuthService implements CanActivate {
 
-  constructor() { }
+  constructor( public firebaseAuth: AngularFireAuth ) { }
 
   canActivate(): boolean {
     console.log('CanActivate was called');
     return true;
+  }
+
+  login() {
+    this.firebaseAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.firebaseAuth.auth.signOut();
   }
 
 }
